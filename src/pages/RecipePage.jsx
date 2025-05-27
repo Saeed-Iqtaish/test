149a6493330139ce5add8a711897c8de0111b124
Saeed-Iqtaish <saeed.ag2004@gmail.com>
2025-05-27 07:06:57 +0300

    home screen implementation

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import RecipeHeader from '../components/recipeDetails/RecipeHeader'
 
 function RecipePage() {
-  return (
-    <RecipeHeader/>
-  )
+    return (
+        <RecipeHeader
+            title={recipe.title}
+            image={recipe.image}
+            mood={assignMood(recipe)}
+        />
+    )
 }
 
 export default RecipePage
